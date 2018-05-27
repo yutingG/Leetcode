ac405add7e1442a9c1b922ea4b90a0b3b0e7743c
yutingGuo <13281030@bjtu.edu.cn>
2018-05-27 17:12:38 +0800

    Accepted 001. Two Sum

--- src/Easy/_001/Solution.java
+++ src/Easy/_001/Solution.java
@@ -1,8 +1,28 @@
 package Easy._001;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
+ * Problem 001. Two Sum
  * @Author: guoyuting
  * @date: 2018-5-25 16:12
  */
 public class Solution {
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        System.out.print(twoSum(nums, target));
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int needNum = target - nums[i];
+            if(map.containsKey(needNum) && map.get(needNum) != i) {
+                return new int[]{map.get(needNum), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
 }
