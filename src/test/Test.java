eb0f47c21099e744f8f2d70be3f3a93571905e7e
yutingGuo <13281030@bjtu.edu.cn>
2018-11-01 14:03:04 +0800

    测试文档

---
+++ src/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @Author: guoyuting
+ * @Date: Created in 2018/11/1 14:01
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
